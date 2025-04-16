48351f09a7ae79ef2f7450ed07a8ec96e3f49013
Jatin Banga <jatin.b.rx3@gmail.com>
2025-04-16 20:01:04 +0530

    initial commit

---
+++ matrx-utils/__init__.py
@@ -0,0 +1,5 @@
+from .core import jatins_print_function
+
+__version__ = "0.1.0"
+
+__all__ = ["jatins_print_function"]
---
+++ matrx-utils/core.py
@@ -0,0 +1,7 @@
+def jatins_print_function(name):
+    print(f"Jatin says hello to {name}")
+
+
+
+
+
